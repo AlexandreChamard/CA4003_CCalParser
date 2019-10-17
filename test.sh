d2694b747e5373e50dc00888000deea5549a0b9e
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2019-10-17 19:39:51 +0100

    fix test.sh to take dirName instead of take number of test

--- test.sh
+++ test.sh
@@ -1,22 +1,28 @@
 
-call="java SyntaxChecker"
+sourceFile="ccalParser.jj"
+programName="SyntaxChecker"
+
+call="java ${programName}"
 error="error"
 testsdir="tests"
 
-javacc sample.jj && javac *.java || exit 1
+javacc "${sourceFile}" && javac *.java || exit 1
 
 file=tmpfile
 
 i=0
+n=0
 
 function test() {
-    local filename="${testsdir}/test${n}/input.txt"
-    local testfile="${testsdir}/test${n}/output.txt"
+    local filename="${f}/input.txt"
+    local testfile="${f}/output.txt"
+
+    n=$((n + 1))
 
     echo 
     echo "============================"
     echo 
-    echo "test${n}: " ${filename}
+    echo "${f}: " ${filename}
     ${call} ${filename} > ${file} && diff -w ${file} ${testfile}
     if [[ ${?} != 0 && "$(< ${testfile})" != "error" ]];then
         i=$((i + 1))
@@ -26,7 +32,7 @@ function test() {
 
 }
 
-for n in $(seq 1 $(find tests/ -maxdepth 1 -mindepth 1 -type d | wc -l)); do
+for f in $(find ${testsdir}/ -maxdepth 1 -mindepth 1 -type d | sort);do
     test
 done
 
