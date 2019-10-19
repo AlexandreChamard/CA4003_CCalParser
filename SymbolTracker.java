8279d2190c8fba5fe5b127f39ec4dac23644eeb9
Alexandre Chamard-bois <alexandre.chamard-bois@epitech.eu>
2019-10-19 20:50:30 +0100

    add SymbolTracker class

---
+++ SymbolTracker.java
@@ -0,0 +1,118 @@
+
+import java.util.*;
+
+class Symbol {
+    String name;
+    String type;
+
+    public Symbol(String _name, String _type) {
+        name = _name;
+        type = _type;
+    }
+
+    public String toString() {
+        return "<"+name+"::"+type+">";
+    }
+}
+
+class SymbolTracker {
+    Hashtable<String, LinkedList<Symbol>> symTracker = new Hashtable<String, LinkedList<Symbol>>();
+    Stack<String> undoStack = new Stack<String>();
+
+    public void addSymbol(String key, Symbol s) throws RuntimeException {
+        if (inCurrentBlock(key) == true)
+            throw new RuntimeException("var " + key + "already instanciated.");
+
+        LinkedList<Symbol> l = symTracker.get(key);
+        if (l == null) {
+            l = new LinkedList<Symbol>();
+            symTracker.put(key, l);
+        }
+        undoStack.push(key);
+        l.push(s);
+    }
+
+    public Symbol getSymbol(String key) throws RuntimeException {
+        LinkedList<Symbol> l = symTracker.get(key);
+
+        if (l == null)
+            throw new RuntimeException("Symbol "+key+" not found.");
+        for (Symbol s : l) {
+            if (s.name == key) {
+                if (l.peek() != s) { // put the found symbol at the top
+                    l.remove(s);
+                    l.push(s);
+                }
+                return s;
+            }
+        }
+        throw new RuntimeException("Symbol "+key+" not found.");
+    }
+
+    protected void remove(String key) throws RuntimeException {
+        LinkedList<Symbol> l = symTracker.get(key);
+
+        for (Symbol s : l) {
+            if (s.name == key) {
+                l.remove(s);
+                return;
+            }
+        }
+        throw new RuntimeException("var doesn't exist.");
+    }
+
+    public void newBlock() {
+        undoStack.push(null);
+    }
+
+    public void deleteBlock() throws RuntimeException {
+        if (undoStack.empty() == true)
+            throw new RuntimeException("destroying an unexisting block.");
+
+        while (undoStack.peek() != null) {
+            remove(undoStack.peek());
+            undoStack.pop();
+        }
+        undoStack.pop();
+    }
+
+    public boolean inCurrentBlock(String key) {
+        for (String s : undoStack) {
+            if (s == key)
+                return true;
+            if (s == null)
+                return false;
+        }
+        return false;
+    }
+
+    protected static String symbolToString(Symbol s) {
+        if (s == null)
+            return "null";
+        return s.toString();
+    }
+
+    public static void main(String[] args) { // SymbolTracker example
+        SymbolTracker st = new SymbolTracker();
+
+        st.newBlock();
+        st.addSymbol("a", new Symbol("a", "int"));
+        st.addSymbol("b", new Symbol("b", "bool"));
+
+        System.out.println("a: "+symbolToString(st.getSymbol("a")));
+        System.out.println("b: "+symbolToString(st.getSymbol("b")));
+
+        st.newBlock();
+
+        st.addSymbol("a", new Symbol("a", "String"));
+
+        System.out.println("a: "+symbolToString(st.getSymbol("a")));
+        System.out.println("b: "+symbolToString(st.getSymbol("b")));
+
+        st.deleteBlock();
+        System.out.println("a: "+symbolToString(st.getSymbol("a")));
+        System.out.println("b: "+symbolToString(st.getSymbol("b")));
+
+        st.deleteBlock();
+    }
+}
